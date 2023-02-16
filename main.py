0099ce7f1ee17511c02c045ca77d2e9524a52131
nathandreuille <nathan.dreuille@sfr.fr>
2023-02-16 19:03:55 +0100

    modification de la fenetre du jeux

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ pygame.init()
 
 # 1 : titre et résolution de la fenetre 
 
-resolution = (640,480)
+resolution = (692,590)
 pygame.display.set_caption("Puissance 4")
 window_screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+damier = pygame.image.load("damier.png")
+damier.convert_alpha()
+red_token = pygame.image.load("pion_rouge.png")
+red_token.convert_alpha()
+yellow_token = pygame.image.load("pion_jaune.png")
+yellow_token.convert_alpha()
 
 #2 : Boucle pour maintenir la fenetre ouverte 
 
@@ -17,3 +23,10 @@ while lauched :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             lauched = False
+    window_screen.blit(damier,[0,0])
+    window_screen.blit(red_token,[100,100])
+    window_screen.blit(yellow_token,[200,200])
+    
+    
+    
+    pygame.display.flip()
