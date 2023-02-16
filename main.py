1d94bb2a5e061f2e8d487546d7d887304aa11d7a
nathandreuille <nathan.dreuille@sfr.fr>
2023-02-16 23:48:57 +0100

    mouvement jeton

--- main.py
+++ main.py
@@ -17,17 +17,59 @@ yellow_token = pygame.image.load("pion_jaune.png")
 yellow_token.convert_alpha()
 
 #2 : Boucle pour maintenir la fenetre ouverte 
-
+x = 0
+y =0
+x2=100
+y2= 100
 lauched = True
 while lauched :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             lauched = False
     window_screen.blit(damier,[0,0])
-    window_screen.blit(red_token,[100,100])
-    window_screen.blit(yellow_token,[200,200])
+    window_screen.blit(yellow_token,(x,y))
+    window_screen.blit(red_token,(x2,y2))
+    pressed = pygame.key.get_pressed()
+    if pressed [pygame.K_LEFT]:
+        x-=1
+        
+    if pressed [pygame.K_RIGHT]:
+        x+=1
+        
+    if pressed [pygame.K_UP]:
+        y-=1
+        
+    if pressed [pygame.K_DOWN]: 
+        y+=1
+        
     
-    
+        
+    if pressed [pygame.K_q]:
+        x2-=1
+        
+    if pressed [pygame.K_d]:
+        x2+=1
+        
+    if pressed [pygame.K_z]:
+        y2-=1
+        
+    if pressed [pygame.K_s]: 
+        y2+=1
     
     pygame.display.flip()
 
+    
+
+    
+    
+
+    
+"""
+class Game :
+    def __init__(self,age,poids):
+        self.age = age
+        self.poids = poids
+    
+    def afficher(self):
+        print("j'ai",self.age,"et je fais",self.poids)
+"""
