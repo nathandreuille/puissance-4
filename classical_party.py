9b913aec23347d7d1267bb364996e6aa01f1597a
nathandreuille <nathan.dreuille@sfr.fr>
2023-03-23 15:13:29 +0100

    uptade 23/03/2023

--- classical_party.py
+++ classical_party.py
@@ -1,17 +1,67 @@
 from tkinter import *
-
+import random 
 fen = Tk()
-fen.resizable(width=False, height=False)
-fen.configure(bg ='#0002FF')
-can = Canvas(fen,width= 692,height= 590, bg ="#0002FF")
-
-fond_image_damier = PhotoImage(file ="image\damier.png" )
-fond_image_pion_rouge = PhotoImage(file ="image\pion_rouge.png" )
-fond_image_pion_jaune = PhotoImage(file ="image\pion_jaune.png" )
-
-can.create_image(345,290,image = fond_image_damier)
-can.create_image(100,100,image = fond_image_pion_rouge)
-can.create_image(100,200,image = fond_image_pion_jaune)
-
+fen.title("partie classique puissance 4")
+fen.resizable(width=False,height= False)
+can = Canvas(fen,height= 600, width=700, bg = "#000EEC")
 can.pack()
+player = random.randint(1,2) # 1 : jaune , 2 : rouge
+
+if player == 1:
+    label = Label(fen,text = "Le joueur jaune commence", font = "arial 16 bold")
+else :   
+    label = Label(fen,text = "Le joueur rouge commence",font = "arial 16 bold")
+label.pack()
+
+
+def checkerboard(): 
+    x1 =0
+    y1 = 0
+    x2 = 100
+    y2 = 100
+    for x in range(6):  
+        for i in range(7):
+            can.create_rectangle(x1,y1,x2,y2)
+            x1+=100
+            x2+=100
+        x1 =0
+        y1+=100
+        y2+=100
+
+
+def red_token(x1,y1,x2,y2):
+    can.create_oval(x1,y1,x2,y2,fill = "red")
+
+
+def yellow_token(x1,y1,x2,y2):
+    can.create_oval(x1,y1,x2,y2,fill = "yellow")
+
+def clic(event):
+    global player
+    X = event.x
+    Y = event.y
+    col = X //100
+    line = Y//100
+    if player == 1:
+        yellow_token(col*100,line*100,col*100+100,line*100+100)
+        player = 2
+    else:
+        red_token(col*100,line*100,col*100+100,line*100+100)
+        player = 1
+
+
+can.bind("<Button>",clic)
+        
+
+
+
+checkerboard()
+
+
+
+
+
 fen.mainloop()
+
+
+
--- main.py
+++ main.py
@@ -5,16 +5,31 @@ windows.title ("puissance 4")
 windows.geometry('400x400+450+250')
 windows.configure(bg ='white')
 
-def classical_party ():
-    damier = PhotoImage(file = "image/damier.png")
-    label_damier = Label (windows,image = damier)
-    label_damier.pack()
-    windows.mainloop()
+def fct_classical_party():
+    fen = Tk()
+    can = Canvas(fen,height= 600, width=700, bg = "#000EEC")
+    can.pack()
+    a =0
+    b = 0
+    c = 100
+    d = 100
+    for x in range(6):  
+        for i in range(7):
+            can.create_rectangle(a,b,c,d)
+            a+=100
+            c+=100
+        a =0
+        b=0
+        b+=100
+        d+=100
+    fen.mainloop()
+
+
 
 can = Canvas(windows, width = 300, height = 300, bg ="#2725BE")
 font = 'arial 13 bold' # ecriture en gras
 boutton_3manches = Button(windows,text = "Commencer une partie en 3 manches",font = font, fg = "black", bg = 'white')
-boutton_classique = Button(windows,text = 'Commencer une partie classique',font = font, fg = "black", bg = 'white',command=classical_party)
+boutton_classique = Button(windows,text = 'Commencer une partie classique',font = font, fg = "black", bg = 'white',command = fct_classical_party )
 quit = Button(windows, text="Annuler", font = font,fg ="black",bg ="white", command = quit )
 frame = Frame (windows, bg = "white")
 label = Label(frame ,text = "Puissance", font = "arial 20 bold", bg = "white", fg = "#FF0000")
