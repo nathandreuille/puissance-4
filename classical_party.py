5b75d43dacf958eaf0928b6739bbc31c93af5d19
nathandreuille <nathan.dreuille@sfr.fr>
2023-03-24 16:10:13 +0100

    update 24 mars 16:10

--- classical_party.py
+++ classical_party.py
@@ -7,7 +7,7 @@ fen.resizable(width=False,height= False)
 can = Canvas(fen,height= 600, width=700, bg = "#000EEC")
 can.pack()
 player = random.randint(1,2) # 1 : jaune , 2 : rouge
-checkerboard_list = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+checkerboard_list = [[0,0,0,0,0,0,3],[0,0,0,0,0,0,3],[0,0,0,0,0,0,3],[0,0,0,0,0,0,3],[0,0,0,0,0,0,3],[0,0,0,0,0,0,3],[0,0,0,0,0,0,3]]
 if player == 1:
     label = Label(fen,text = "Le joueur jaune commence", font = "arial 16 bold")
 else :   
@@ -35,7 +35,7 @@ def red_token(x1,y1,x2,y2):
 
 
 def yellow_token(x1,y1,x2,y2):
-    can.create_oval(x1+10,y1+10,x2-10,y2-10,fill = "#EED430",width=3)
+    can.create_oval(x1+10,y1+10,x2-10,y2-10,fill = "#4DFF00",width=3)
 
 def clic(event):
     global player,checkerboard_list
@@ -43,19 +43,28 @@ def clic(event):
     Y = event.y
     col = X //100
     line = Y//100
+    id_checkerboard = [col,line]
+    
     if player == 1:
+        while checkerboard_list[col][line+1]==0:
+            line+=1
+
+        checkerboard_list[col][line] = 1
         yellow_token(col*100,line*100,col*100+100,line*100+100)
         player = 2
+        
     else:
+        while checkerboard_list[col][line+1]==0:
+            line+=1
+        checkerboard_list[col][line] = 2
         red_token(col*100,line*100,col*100+100,line*100+100)
         player = 1
-    
-
 
 
 can.bind("<Button>",clic)
 checkerboard()
 fen.mainloop()
+
 
 
 
--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 from tkinter import *
-
+#from classical_party import Game
 windows = Tk()
 windows.title ("puissance 4")
-windows.geometry('400x400+450+250')
-windows.configure(bg ='white')
-
-def fct_classical_party():
-    fen = Tk()
-    can = Canvas(fen,height= 600, width=700, bg = "#000EEC")
-    can.pack()
-    a =0
-    b = 0
-    c = 100
-    d = 100
-    for x in range(6):  
-        for i in range(7):
-            can.create_rectangle(a,b,c,d)
-            a+=100
-            c+=100
-        a =0
-        b=0
-        b+=100
-        d+=100
-    fen.mainloop()
-
-
-
-can = Canvas(windows, width = 300, height = 300, bg ="#2725BE")
+windows.geometry('600x600+450+250')
+windows.configure(bg ='#0586C6')
+can = Canvas(windows, width = 500, height = 500, bg ="#2725BE")
 font = 'arial 13 bold' # ecriture en gras
 boutton_3manches = Button(windows,text = "Commencer une partie en 3 manches",font = font, fg = "black", bg = 'white')
-boutton_classique = Button(windows,text = 'Commencer une partie classique',font = font, fg = "black", bg = 'white',command = fct_classical_party )
+boutton_classique = Button(windows,text = 'Commencer une partie classique',font = font, fg = "black", bg = 'white')
 quit = Button(windows, text="Annuler", font = font,fg ="black",bg ="white", command = quit )
 frame = Frame (windows, bg = "white")
-label = Label(frame ,text = "Puissance", font = "arial 20 bold", bg = "white", fg = "#FF0000")
-label2 = Label(frame, text = "4", font = "arial 20 bold", bg = "white", fg = "#FFF000")
+fond_image = PhotoImage(file = "image/1.png")
+#label = Label(frame ,text = "Puissance", font = "arial 20 bold", bg = "white", fg = "#FF0000")
+#label2 = Label(frame, text = "4", font = "arial 20 bold", bg = "white", fg = "#FFF000")
+can.create_image(0,10, image = fond_image)
 can.pack()
 boutton_classique.pack()
 boutton_3manches.pack()
 can.bind( )
-label.pack()
-label2.pack()
+#label.pack()
+#label2.pack()
 frame.place(x = 130 , y = 130)
 quit.pack()
 windows.mainloop()
